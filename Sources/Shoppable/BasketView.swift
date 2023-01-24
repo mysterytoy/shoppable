4542c052a786cb92f6f09d6bd5c7840591692162
Edward Toy <edward.toy@pm.me>
2023-01-24 14:44:47 +0100

    Show total on BasketView

--- Sources/Shoppable/BasketView.swift
+++ Sources/Shoppable/BasketView.swift
@@ -5,20 +5,50 @@ import SwiftUI
 struct BasketView: View {
   @ObservedObject var viewModel: AppViewModel
   
+  @Environment(\.colorScheme) var colorScheme
+  
   var body: some View {
-    List(Array(viewModel.basket)) { product in
-      HStack {
-        VStack(alignment: .leading) {
-          Text(product.name)
+    ZStack {
+      List(Array(viewModel.basket)) { product in
+        HStack {
+          VStack(alignment: .leading) {
+            Text(product.name)
+              .font(.headline)
+            Text(product.price.description)
+              .font(.subheadline)
+              .foregroundColor(.secondary)
+          }
+          Spacer()
+          Button(action: { viewModel.remove(product) }) {
+            Text("Remove")
+              .foregroundColor(.red)
+          }
+        }
+      }
+      VStack {
+        Spacer()
+        VStack {
+          Text("Total")
             .font(.headline)
-          Text(product.price.description)
+          Text("\(viewModel.total)")
             .font(.subheadline)
             .foregroundColor(.secondary)
         }
-        Spacer()
-        Button(action: { viewModel.remove(product) }) {
-          Text("Remove")
-        }
+        .padding()
+        .background(
+          colorScheme == .dark
+            ? Color.white.opacity(0.2)
+            : Color.white
+        )
+        .cornerRadius(10)
+        .overlay(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(
+              Color.black.opacity(0.1),
+              lineWidth: 1
+            )
+        )
+        .padding()
       }
     }
     .navigationTitle("Basket")
